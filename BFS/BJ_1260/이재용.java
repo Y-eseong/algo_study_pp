3e49721436607d041444f35e3d43f30257706bc4
Jaeyong Lee <jaeyong9085@gmail.com>
2025-07-24 21:58:03 +0900

    Update 이재용.java
    - 인접 행렬 -> 인접 리스트로 저장 형식 변경: 탐색 속도 개선
    - Stack, Queue -> Deque로 구현: 속도, 안정성, 일관성 개선

--- BFS/BJ_1260/이재용.java
+++ BFS/BJ_1260/이재용.java
@@ -1,77 +1,73 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, V;
-	static int[][] matrix;
-	
-	static Stack<Integer> stack = new Stack<>();
-	static Queue<Integer> queue = new LinkedList<>();
-	
-	static boolean[] stack_visited;
-	static boolean[] queue_visited;
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		V = Integer.parseInt(st.nextToken());
-		
-		matrix = new int[N + 1][N + 1];
-		stack_visited = new boolean[N + 1];
-		queue_visited = new boolean[N + 1];
+		ArrayList<Integer>[] adjList = new ArrayList[N + 1];
+		for (int i = 0; i <= N; i++) {
+			adjList[i] = new ArrayList<>();
+		}
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int s = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
-			matrix[s][d] = 1;
-			matrix[d][s] = 1;
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			adjList[start].add(end);
+			adjList[end].add(start);
 		}
-		dfs(V);
-		bfs(V);
-	}
-	static void dfs(int V) {
-	    StringBuilder sb = new StringBuilder();
-
-	    stack.push(V);
-
-	    while (!stack.isEmpty()) {
-	        int current = stack.pop();
-	        if (stack_visited[current]) continue;
-
-	        stack_visited[current] = true;
-	        sb.append(current).append(" ");
-
-	        for (int i = N; i >= 1; i--) {
-	            if (matrix[current][i] == 1 && !stack_visited[i]) {
-	                stack.push(i);
-	            }
-	        }
-	    }
-	    System.out.println(sb);
-	}
-	static void bfs(int V) {
+		
+		// 인접 리스트로 구현 시 정렬 필수!!
+		for (int i = 1; i <= N; i++) {
+			Collections.sort(adjList[i]);
+		}
+		
+		Deque<Integer> stack = new ArrayDeque<>();
+		boolean[] stack_visited = new boolean[N + 1];
+		
+		stack.push(V);
 		StringBuilder sb = new StringBuilder();
+		while(!stack.isEmpty()) {
+			int current = stack.pop();
+			if (!stack_visited[current]) {
+				stack_visited[current] = true;
+				sb.append(current).append(" ");
+				
+				for (int i = adjList[current].size() - 1; i >= 0; i--) {
+					if (!stack_visited[adjList[current].get(i)]) {
+						stack.push(adjList[current].get(i));
+					}
+				}
+			}
+		}
+		System.out.println(sb);
+		
+		Queue<Integer> queue = new ArrayDeque<>();
+		boolean[] queue_visited = new boolean[N + 1];
 		
 		queue.offer(V);
-		
+		sb = new StringBuilder();
 		while(!queue.isEmpty()) {
 			int current = queue.poll();
-			if (queue_visited[current]) continue;
-			
-			queue_visited[current] = true;
-			sb.append(current).append(" ");
-			
-			for (int i = 1; i <= N; i++) {
-				if (matrix[current][i] == 1 && !queue_visited[i]) {
-					queue.offer(i);
+			if(!queue_visited[current]) {
+				queue_visited[current] = true;
+				sb.append(current).append(" ");
+				
+				for (int destination : adjList[current]) {
+					if(!queue_visited[destination]) {
+						queue.offer(destination);
+					}
 				}
 			}
 		}
