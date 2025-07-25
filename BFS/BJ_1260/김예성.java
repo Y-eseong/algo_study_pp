9ca2436a48891107c88e7727c8378db00c8e4fec
Y-eseong <yodeng94@naver.com>
2025-07-25 13:03:39 +0900

    2트

--- BFS/BJ_1260/김예성.java
+++ BFS/BJ_1260/김예성.java
@@ -1,96 +1,61 @@
-package com.ssafy.test;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	
-	static class Pair{
-		int x;
-		int y;
+	static boolean[] visited; 
+	static StringBuilder sb = new StringBuilder();
+	static int[][] graph;
+	static int n, m, v;
+	static Queue<Integer> q = new LinkedList<>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		n = Integer.parseInt(input[0]);
+		m = Integer.parseInt(input[1]);
+		v = Integer.parseInt(input[2]);
+		visited = new boolean[n+1];
+		graph = new int[n+1][n+1];
 		
-		public Pair(int x, int y) {
-			this.x = x;
-			this.y = y;
+		for(int i = 0; i < m; i++) {
+			String[] in = br.readLine().split(" ");
+			int a = Integer.parseInt(in[0]);
+			int b = Integer.parseInt(in[1]);
+			graph[a][b] = 1;
+			graph[b][a] = 1;
 		}
+		dfs(v);
+		sb.append("\n");
+		visited = new boolean[n+1];
+		bfs(v);
+		System.out.println(sb);
+	}
+	
+	static void dfs(int next) {
+		visited[next] = true;
+		sb.append(next).append(" ");
 		
-		public int getX() {
-			return x;
-		}
-		
-		public int getY() {
-			return y;
+		for(int i = 1; i <=n; i++) {
+			if(!visited[i] && graph[next][i] == 1) {
+				dfs(i);
+			}
 		}
 	}
-	// 상 하 좌 우
-	static int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 	
-	static Queue<Pair> q = new LinkedList<>();
-	
-    public static void main(String args[]) throws IOException {
-    	// 읽어오기
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i = 1; i <= T; i++) {
-        	// m 가로, n 세로, k 배추
-        	String[] input = br.readLine().split(" ");
-        	int m = Integer.parseInt(input[0]);
-        	int n = Integer.parseInt(input[1]);
-        	int k = Integer.parseInt(input[2]);
-        	int[][] ground = new int[n][m];
-        	boolean[][] visited = new boolean[n][m]; // 이미 방문했으면 q에 넣지 않기
-        	// 지렁이 수
-        	int count = 0;
-        	
-        	for(int j = 0; j < k; j++) {
-        		String[] in = br.readLine().split(" ");
-        		int y = Integer.parseInt(in[0]);
-        		int x = Integer.parseInt(in[1]);
-        		// 배추 있는 곳 1 나머지 0
-        		ground[x][y] = 1;
-        	}
-
-        	for(int x = 0; x < n; x ++) {
-        		for(int y = 0; y < m; y++) {
-        			if(ground[x][y] == 1) {
-        				q.offer(new Pair(x, y));
-        				visited[x][y] = true;
-        				bfs(ground, visited, n, m);
-        				count++;
-        			}
-        		}
-        	}
-        	sb.append(count).append("\n");
-        }
-        System.out.println(sb);
-    }
-    
-	static void bfs(int[][] ground, boolean[][] visited, int n, int m) {
-		
-		while(!q.isEmpty()){			
-			Pair cur = q.poll();
-			int cx = cur.getX();
-			int cy = cur.getY();
-			ground[cx][cy] = 0;
-			
-			for(int[] dxy : dir) {
-				int nx = cx + dxy[0];
-				int ny = cy + dxy[1];
-				
-				if(nx < 0 || ny < 0 || nx >= n || ny >= m) {
-					continue;
-				}
-				
-				if(ground[nx][ny] == 1) {
-					if(visited[nx][ny]) {
-						continue;
-					}
-					q.offer(new Pair(nx, ny));
-					visited[nx][ny] = true;
+	static void bfs(int next) {
+		q.offer(next);
+		visited[next] = true;
+		while(!q.isEmpty()) {
+			next = q.poll();
+			sb.append(next).append(" ");
+			for(int i = 1; i <= n; i++) {
+				if(!visited[i] && graph[next][i] == 1) {
+					q.offer(i);
+					visited[i] = true;
 				}
 			}
 		}
+		
 	}
 }
